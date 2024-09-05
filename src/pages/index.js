686709b031202c20574aba2e22119be4d610854e
Sean Conroy <sean@econroy.com>
2024-09-04 23:22:32 -0700

    adjust alignment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,11 @@
-
-import localFont from "next/font/local";
 import { LatencyCalculator } from "../components/latency_calculator"; // Make sure the path is correct
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-
-        {/* Add the LatencyCalculator component here */}
+    <div className="min-h-screen flex flex-col justify-start items-center p-8 pb-20 sm:p-20 font-sans">
+      <main className="flex flex-col gap-8 items-center sm:items-start mt-16">
         <LatencyCalculator />
       </main>
-      
     </div>
   );
 }
